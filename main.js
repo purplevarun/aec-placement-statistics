6d13e8793872c3288a2a260c42887f1ea74233ee
purplevarun <purplevarun@gmail.com>
2021-10-08 20:55:36 +0530

    mongoose se limked

--- main.js
+++ main.js
@@ -17,7 +17,16 @@ app.post("/add", (req, res) => {
   const name = req.body.name;
   const company = req.body.company;
   const ctc = req.body.ctc;
-  console.log(name, company, ctc);
+  const newStudent = new Student({
+    name: name,
+    company: company,
+    ctc: ctc + " LPA",
+  });
+  newStudent.save((err) => {
+    if (err) throw err;
+    console.log("new student saved");
+  });
+  console.log(newStudent);
   res.render("thanks", { name: name });
 });
 app.get("/add", (req, res) => {
