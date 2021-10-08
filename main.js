9322135600d29b083d310e1d9d0dbdefc44307e8
purplevarun <purplevarun@gmail.com>
2021-10-08 19:07:18 +0530

    basic stuff

---
+++ main.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.get("/home", (req, res) => {
+  res.render("");
+});
+app.get("/", (req, res) => {
+  app.redirect("/home");
+});
+app.listen(port, () => console.log("app hosted at", port));
