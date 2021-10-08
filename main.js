aacbc1fb1b18826021c8924b15c320141e462420
purplevarun <purplevarun@gmail.com>
2021-10-08 20:43:32 +0530

    doing it

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const mongoose = require("mongoose");
+const Student = require("./models/student");
+require("dotenv").config();
+mongoose.connect(process.env.DB_URL, (err) => {
+  if (err) {
+    throw err;
+  }
+  console.log("database connected");
+});
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.post("/add", (req, res) => {
+  const name = req.body.name;
+  const company = req.body.company;
+  const ctc = req.body.ctc;
+  console.log(name, company, ctc);
+});
+app.get("/add", (req, res) => {
+  res.render("addStudent");
+});
 app.get("/home", (req, res) => {
   res.render("home");
 });
